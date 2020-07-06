0f578bc91c5f138c6d8073cca2332ebeadbf6b3c
AnderBros <38515563+AnderBros@users.noreply.github.com>
2020-07-05 22:26:41 -0400

    Creación de storyboard

---
+++ FinalTestIOS/Classes/ Discs/DiscViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DiscViewController.swift
+//  FinalTestIOS
+//
+//  Created by user171789 on 7/5/20.
+//  Copyright © 2020 user171789. All rights reserved.
+//
+
+import UIKit
+
+class DiscViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
